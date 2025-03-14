1074997774067f491ba3e1e63a372ccf355dccae
whatanakKean <85047164+whatanakKean@users.noreply.github.com>
2025-03-14 09:45:38 +0000

    DataByGroup

--- frontend/src/pages/DataByGroup/DataByGroup.tsx
+++ frontend/src/pages/DataByGroup/DataByGroup.tsx
@@ -1,10 +1,71 @@
 // src/pages/Home.tsx
 import React from 'react';
 import { Grid, Select, Accordion, AccordionItem, AccordionControl, AccordionPanel, Group, Text, Paper } from '@mantine/core';
+import ReactECharts from 'echarts-for-react';
+
 
 const DataByGroup: React.FC = () => {
+    const option = {
+        title: {
+            text: 'Stacked Area Chart'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['Email Marketing', 'Affiliate Ads', 'Video Ads']
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category', // Correctly specify 'category' here
+                boundaryGap: false,
+                data: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: 'Email Marketing',
+                type: 'line',
+                stack: 'Total',
+                areaStyle: { normal: {} },
+                data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+                name: 'Affiliate Ads',
+                type: 'line',
+                stack: 'Total',
+                areaStyle: { normal: {} },
+                data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+                name: 'Video Ads',
+                type: 'line',
+                stack: 'Total',
+                areaStyle: { normal: {} },
+                data: [150, 232, 201, 154, 190, 330, 410]
+            }
+        ]
+    };
+    
+
   return (
-    <Grid>
+    <Grid p={10}>
       <Grid.Col span={{ base: 12, sm: 3 }}>
         <Paper p="md" radius="md" shadow="xs" withBorder>
         <Select
@@ -100,7 +161,15 @@ const DataByGroup: React.FC = () => {
         </Accordion>
         </Paper>
       </Grid.Col>
-      <Grid.Col span={{ base: 12, sm: 9 }}>2</Grid.Col>
+      <Grid.Col span={{ base: 12, sm: 9 }}>
+        <Paper p="md" radius="md" shadow="xs" withBorder>
+            <ReactECharts
+                option={option}
+                style={{ height: 400 }}
+            />
+        </Paper>
+        
+      </Grid.Col>
     </Grid>
   );
 };
