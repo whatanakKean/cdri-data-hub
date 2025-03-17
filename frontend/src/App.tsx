d13eb68bd7d0c5e80d9f8715c8a6a9a660392509
whatanakKean <85047164+whatanakKean@users.noreply.github.com>
2025-03-17 09:27:55 +0000

    Fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,37 @@
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import Home from './pages/Home/Home';
 import { HeaderMenu } from './components/Header/HeaderMenu';
-
 import { FooterMenu } from './components/Footer/FooterMenu';
 import { NotFound } from './components/NotFound/NotFound';
 import DataByGroup from './pages/DataByGroup/DataByGroup';
-
+import { IconArrowUp } from '@tabler/icons-react';
+import { ActionIcon, Affix } from '@mantine/core';
 
 const App: React.FC = () => {
+  // Function to scroll to top
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <MantineProvider>
       <Router>
-      <HeaderMenu />
+        <HeaderMenu />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/data" element={<DataByGroup />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      <FooterMenu />
+        <FooterMenu />
+        
+        <Affix position={{ bottom: 20, right: 20 }}>
+          <ActionIcon color="blue" radius="xl" size={50} onClick={scrollToTop}>
+            <IconArrowUp stroke={1.5} size={30} />
+          </ActionIcon>
+        </Affix>
       </Router>
     </MantineProvider>
   );
